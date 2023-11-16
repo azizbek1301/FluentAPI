9db630d99b128f4b9274c5fda074307eee762f89
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-16 18:44:31 +0500

    person model added

---
+++ Fluent API/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace Fluent_API.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Car> Cars { get; set; }
+    }
+}

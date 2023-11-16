c3ace30a9cf860d9cf1e75ab1eb82d9985552aae
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-16 18:47:26 +0500

    Person model changed

--- Fluent API/Models/Person.cs
+++ Fluent API/Models/Person.cs
@@ -3,7 +3,7 @@
     public class Person
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public ICollection<Car> Cars { get; set; }
+        public string? Name { get; set; }
+        public ICollection<Car>? Cars { get; set; }
     }
 }

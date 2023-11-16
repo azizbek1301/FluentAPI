5a97cd93da7f51f2ec7e187c63a14e8c245612b7
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-16 18:44:20 +0500

    Cars model added

---
+++ Fluent API/Models/Car.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Fluent_API.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int PersonInfoId {  get; set; }
+        
+        [ForeignKey("PersonInfoId")]
+        public Person? Person { get; set; }
+    }
+}

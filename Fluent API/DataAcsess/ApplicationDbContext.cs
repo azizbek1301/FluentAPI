0814dca31223add6003e992e289477334539c443
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-16 18:43:52 +0500

    DataAccess changed

---
+++ Fluent API/DataAcsess/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Fluent_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fluent_API.DataAcsess
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+        public virtual DbSet<Person>Persons { get; set; }
+        public virtual DbSet<Car> Cars { get; set; }
+    }
+}

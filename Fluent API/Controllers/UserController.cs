ae1931eede47df1760077b4b14754d7eb51fa0d5
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-11-16 18:43:34 +0500

    User Controller Added

---
+++ Fluent API/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Fluent_API.DataAcsess;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fluent_API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+        public UserController(ApplicationDbContext applicationDbContext)
+        {
+            _context = applicationDbContext;
+
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetNimadur()
+        {
+            var result = await _context.Persons
+                .Include(x=>x.Cars)
+                .ToListAsync();
+            return Ok(result);
+        }
+    }
+}
